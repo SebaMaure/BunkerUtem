84849ca089750dbef4ab47c22adfe0b4c54ade38
SebaMaure <seba_maure@hotmail.com>
2012-11-08 18:30:04 -0300

    dasdas

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>VideoClub "Frodo"</title>
+<style type="text/css">
+<!--
+#Layer1 {
+	position:absolute;
+	left:629px;
+	top:640px;
+	width:240px;
+	height:210px;
+	z-index:1;
+}
+#Layer2 {
+	position:absolute;
+	left:447px;
+	top:300px;
+	width:239px;
+	height:1692px;
+	z-index:2;
+}
+#Layer3 {
+	position:absolute;
+	left:284px;
+	top:340px;
+	width:168px;
+	height:164px;
+	z-index:1;
+}
+#Layer4 {
+	position:absolute;
+	left:350px;
+	top:66px;
+	width:496px;
+	height:104px;
+	z-index:2;
+}
+#Layer5 {
+	position:absolute;
+	left:904px;
+	top:178px;
+	width:209px;
+	height:83px;
+	z-index:3;
+}
+#Layer6 {
+	position:absolute;
+	left:939px;
+	top:100px;
+	width:280px;
+	height:105px;
+	z-index:3;
+}
+#Layer7 {
+	position:absolute;
+	left:1013px;
+	top:143px;
+	width:64px;
+	height:64px;
+	z-index:4;
+}
+.Estilo1 {font-family: "Comic Sans MS"}
+
+-->
+a.navred:link { text-decoration: none; color: #000077; font-family: arial, calibri, verdana; font-size: 16px; font-weight: 100; }
+a.navred:visited { text-decoration: none; color: #000077; font-family: arial, calibri, verdana; font-size: 16px; font-weight: 100; }
+a.navred:hover { text-decoration: none; color: #ffffff; font-family: arial, calibri,verdana; font-size: 16px; font-weight: 100; }
+
+</style>
+
+</head>
+
+<body link=white vlink=white alink=white bgcolor=#bbbbbb>
+<table border=0 align=center width=1000 bgcolor=#ffffff cellpadding=0 cellspacing=0>
+<tr><td>
+<img src="imagenes/bienvenido.jpg"><br><br>
+</td></tr>
+<tr><td>
+
+ 
+  <?php
+ 
+ 
+    echo '<b><u><font face="Comic Sans MS"></font></u></b>
+	
+	
+	<table border=0 align=left cellpadding=0 cellspacing=0>
+	<tr valign=top><td>
+		<table border=0 width=253 cellpadding=0 cellspacing=0>
+		<tr><td width=253 height=65 background="imagenes/cuadro_verde.jpg" align=center>
+		    <a href="registro_pelicula.php" class="navred"> Registrar pel&iacute;cula </a>
+		</td></tr><tr><td width=253 height=65 background="imagenes/cuadro_verde2.jpg" align=center>
+		    <a href="registro_socio.php" class="navred"> Registrar nuevo cliente </a>
+		</td></tr><tr><td width=253 height=65 background="imagenes/cuadro_verde.jpg" align=center>
+		    <a href="registro_arriendo.php" class="navred"> Arriendos </a>
+		</td></tr><tr><td width=253 height=65 background="imagenes/cuadro_verde2.jpg" align=center>
+		    <a href="genero.php" class="navred"> Genero </a>
+		</td></tr><tr><td width=253 height=65 background="imagenes/cuadro_verde.jpg" align=center>
+		    <a href="formato.php" class="navred"> Formato</a>
+		</td></tr><tr><td width=253 height=65 background="imagenes/cuadro_verde2.jpg" align=center>
+		    <a href="Registro_Estudio.php" class="navred"> Agregar Estudio de Grabacion </a>
+		</td></tr>
+		</table>
+	</td>
+	<td width=490><center>
+		  <img src="imagenes/video_club.gif"><br><br><br><img src="imagenes/logo.jpg"><br><br><br>
+		<font face="arial narrow" size=3 color=#777777>	VideoClub Frodo<br> Luis Barros Borgono 574 - El Bosque<br>09 749 53 42 - (02) 527 91 45<br><br> </font>
+
+</center>
+	</td>
+	<td>
+		<table border=0 width=253 cellpadding=0 cellspacing=0>
+		<tr><td width=253 height=65 background="imagenes/cuadro_verde.jpg" align=center>
+		  <a href="Registro_Director.php" class="navred"> Agregar nuevo Director </a>
+		</td></tr><tr><td width=253 height=65 background="imagenes/cuadro_verde2.jpg" align=center>
+		  <a href="Registro_Actor.php" class="navred"> Agregar Actor </a>
+		</td></tr><tr><td width=253 height=65 background="imagenes/cuadro_verde.jpg" align=center>
+		  <a href="Registro_Personaje.php" class="navred"> Agregar Personaje Actor </a>
+		</td></tr><tr><td width=253 height=65 background="imagenes/cuadro_verde2.jpg" align=center>
+		  <a href="Registro_Distribuidor.php" class="navred"> Agregar Distribuidor </a>
+		</td></tr><tr><td width=253 height=65 background="imagenes/cuadro_verde.jpg" align=center>
+		  <a href="ver_pelicula.php" class="navred"> Ver Pelicula </a>
+		</td></tr><tr><td width=253 height=65 background="imagenes/cuadro_verde2.jpg" align=center>
+		  <a href="ver_pelicula.php" class="navred"> Salir </a>
+	</td></tr>
+	</table>	  
+
+
+</td></tr>
+</table>
+</body>
+</html>
